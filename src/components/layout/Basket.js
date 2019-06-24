6daa97db1e5ff55fb0825252a7f8cce7c4ba8e6f
Aiperishaaa <aiperishaaa@gmail.com>
2019-06-24 15:51:09 +0600

    trial commit of peri branch

---
+++ src/components/layout/Basket.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Oven extends Component {  
+    render(){
+      return (
+        <div>
+            <h2>ВОСХОДЯЩИЙ ОВЕН</h2>          
+        </div>              
+      )
+    }
+  }
+export default Oven
