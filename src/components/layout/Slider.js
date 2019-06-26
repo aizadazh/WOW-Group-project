8356c7782afc1b99d43d84617a39c0cd1ff07fc1
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-06-26 14:39:53 +0600

    slider has started

---
+++ src/components/layout/Slider.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import b1 from "../../static/images/banner1.jpg";
+import b2 from "../../static/images/banner2.jpg";
+import b3 from "../../static/images/banner3.jpg";
