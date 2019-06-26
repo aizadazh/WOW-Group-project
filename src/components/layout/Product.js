9d092b301795a917ddc64d9e2380752e568450f2
danizat <dbaiyshova@gmail.com>
2019-06-26 11:18:22 +0600

    Product

---
+++ src/components/layout/Product.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Product extends Component {  
+    render(){
+      return (
+        <div>
+            <h2>Product Page</h2>          
+        </div>              
+      )
+    }
+  }
+export default Product
