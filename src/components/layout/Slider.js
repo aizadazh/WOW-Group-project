cf2724f49399f595f17b8f853aad69600355239d
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-06-26 14:44:44 +0600

    created files in page folder

--- src/components/layout/Slider.js
+++ src/components/layout/Slider.js
@@ -3,3 +3,23 @@ import Slider from "react-slick";
 import b1 from "../../static/images/banner1.jpg";
 import b2 from "../../static/images/banner2.jpg";
 import b3 from "../../static/images/banner3.jpg";
+import { Container } from "react-bootstrap";
+
+class Slider extends Component {
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+      };
+      return (
+        <Container>
+
+        </Container>
+
+
+export default Slider;
