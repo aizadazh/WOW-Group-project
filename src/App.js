15ad49a61af9923d4709eca957edd7807e5e43a8
Aiperishaaa <aiperishaaa@gmail.com>
2019-06-27 20:49:13 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./pages/main";
 
+
 class App extends React.Component {
   render() {
     return (
---
+++ src/components/layout/Questions.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+class Questions extends Component {
+      render() {
+        return (
+          <Container>
+            <h1>Вопросы и ответы</h1>
+          </Container>
+        )
+      };
+    }
+  
+  export default Questions;
