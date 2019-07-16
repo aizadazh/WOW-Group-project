f32fd28a6a74ae4c40ba565e7025ceaaf22f0915
danizat <dbaiyshova@gmail.com>
2019-07-16 16:28:05 +0600

    product page

--- src/components/elements/ProductSmallSlider.js
+++ src/components/elements/ProductSmallSlider.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import {Col} from "styled-bootstrap-grid";
 import OwlCarousel from 'react-owl-carousel';
-import b1 from '../../static/images/banner2.jpg';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import ReactImageZoom from 'react-image-zoom';
+import Ratings from "./Ratings";
+
+import b1 from '../../static/images/banner1.jpg';
 
 const SliderWrapper = styled.div`
 	display: flex;
-	flex-direction: column;
-	align-items: center;
-	z-index: 999999;
-	justify-content: space-around;
+  flex-direction: column;
+  align-items: center;	
+	z-index: 1000;
+	margin:0 auto;
+  justify-content: space-around;
 `;
 const CarouselItems = styled.div`
-	border:1px solid green;
+		width: 114px;
+	overflow: hidden;   
 	border-radius: 5px;
-	overflow: hidden;
 
 `;
 
 const Preview = styled.div`
-	width:500px;
-	height:500px;
+width:21em;
+	height:21em;
 	backgound:#333;
+	margin:0 auto;
+	padding: 0 auto; 
+	${ReactImageZoom} {
+	
+		border:5px solid green;
+	}
 `;
 const options	 = {
 	margin:10,
@@ -28,16 +41,13 @@ const options	 = {
 	center:true,
 	responsive:{
 		0:{
-				items:2,
-				margin:5
+			items:3,
 		},
 		600:{
-				items:3,
-				margin:10
+			items:4,
 		},
 		1000:{
-				items:3,
-				margin:15
+			items:3,
 
 		}
 }
@@ -47,9 +57,10 @@ class ProductSmallSlider extends Component {
 
 
 	render() {
-	
+		const previewImageProps = {width: 400, height: 400, zoomWidth: 400, img: b1}
 		return (
-			
+			<Col xl= {5} lg= {5} md ={12} sm = {12}>
+
 				<SliderWrapper>
 					<OwlCarousel
 							className="owl-theme"
@@ -57,15 +68,22 @@ class ProductSmallSlider extends Component {
 							nav
 							{...options}
 					>
-							<CarouselItems className="item"><img src = {b1} alt = "productKinds"/>></CarouselItems>
+							<CarouselItems className="item"><img src = {b1} alt = "productKinds"/></CarouselItems>
 							<CarouselItems className="item"><img src = {b1} alt = "productKinds"/></CarouselItems>
 							<CarouselItems className="item"><img src = {b1} alt = "productKinds"/></CarouselItems>
 							<CarouselItems className="item"><img src = {b1} alt = "productKinds"/></CarouselItems>
 						
 					</OwlCarousel>
-					<Preview></Preview>
-				</SliderWrapper>
-		
+					<Preview>
+						<ReactImageZoom 
+						{...previewImageProps}
+						/>
+					</Preview>
+
+				</SliderWrapper>	
+				<Ratings/>
+
+		</Col>
 		);
 	}
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {Container, Row} from "styled-bootstrap-grid";
 import {Layout} from "../components";
-import {ProductSmallSlider} from "../components/elements";
+import {ProductSmallSlider,ProductInfo} from "../components/elements";
 
 
 
@@ -9,13 +9,14 @@ class Product extends Component {
     render(){
       return (
 				<Layout>
-          {/* <Container>
-          <Row> */}
+          <Container>
+          <Row>
            
                 <ProductSmallSlider/>
+                <ProductInfo/>
                
-          {/* </Row>  
-          </Container>  */}
+          </Row>  
+          </Container> 
 				</Layout>
        
       );
