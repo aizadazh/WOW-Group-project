385e052f054a912b17f8b233847c99e84362823b
Aiperishaaa <aiperishaaa@gmail.com>
2019-07-25 17:32:19 +0600

    test Postlist

--- src/components/layout/PostList.js
+++ src/components/layout/PostList.js
@@ -1,17 +1,17 @@
 import React, { Component} from 'react'
-import PostData from '../data/posts.json'
+//import PostData from '../data/posts.json'
 
 class PostList extends Component {
     render () {
         return (
             <div>
                 <h1>Agreements</h1>
-                {PostData.map((postDetail, index) => {
+                {/* {PostData.map((postDetail, index) => {
                     return <div>
                         <h1>{postDetail.title}</h1>
                         <h1>{postDetail.content}</h1>
                     </div>
-                })}
+                })} */}
             </div>
         )
     }
