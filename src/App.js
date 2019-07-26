8998b3a63156e2f8f54d7df90b5af94f6c89b7bc
Aiperishaaa <aiperishaaa@gmail.com>
2019-07-26 14:13:19 +0600

    testing page Agreement

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import BasketPage from "./pages/Basket";
 import FavoritesPage from "./pages/Favorites";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-//import PostList from "./components/layout/PostList";
+import PostList from "./components/layout/PostList";
 
 class App extends React.Component {
   render() {
@@ -26,7 +26,7 @@ class App extends React.Component {
           <Route path="/Favorites" exact component={FavoritesPage} />
           <Route path="/Register"  component={Register} />
           <Route path="/Login"  component={Login} />
-          {/* <Route path="/PostList"  component={PostList} /> */}
+          <Route path="/PostList"  component={PostList} />
         </>        
       </Router>
     );
