226959f63a87079221be39bc06ac646d73604fc9
Aiperishaaa <aiperishaaa@gmail.com>
2019-09-04 15:29:11 +0600

    test REST API

---
+++ src/components/layout/Qa.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class Qa extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://wow.kg/crm/api/?action=list&object=api_articles')
+        .then( result => result.json())
+        .then( json => {
+            this.setState({
+                items:json,
+                isLoaded: true
+            })
+        });
+    }
+
+    render() {
+        var {isLoaded, items} = this.state;
+
+        if ( !isLoaded ) {
+            return (
+                <div>Loading...</div>
+            );
+        } else {
+            return (
+                <div className="Qa">
+                  <ul>
+                      {items.map(item => (
+                          <li><a href={item.link}>{item.title.rendered}</a></li>
+                      ))}
+                  </ul>
+                </div>
+            );
+        }
+    }
+}
+
+export default Qa;
