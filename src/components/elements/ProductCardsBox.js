13835742d1bab8e16fc020f420cfa2d6e1506b15
nuri-kani <nurilaa@mail.ru>
2019-09-04 15:14:55 +0600

    ProductCardBox fetch

--- src/components/elements/ProductCardsBox.js
+++ src/components/elements/ProductCardsBox.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Col} from "styled-bootstrap-grid";
-import CategoryCard from "./CategoryСard";
+//import CategoryCard from "./CategoryСard";
 import DropdownCategory from "./DropdownCategory";
 import { colors} from "../../Config/Var";
-import CategoryData from "../../data/category.json";
+//import CategoryData from "../../data/category.json";
 
 
 const MainProductBox = styled.div`
@@ -18,27 +18,69 @@ const ProductBox = styled.div`
     flex-wrap: wrap;
 `;
 
+
+
 export default class ProductCardBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+  
+    componentDidMount() {
+        fetch(`http://wow.kg/crm/api/?action=view&object=api_products&id=64/get`)
+        .then(response => response.json())
+        .then(json => { 
+            this.setState({
+                isLoaded: true,
+                items: json.id,
+            })
+        });
+}
     render() {
-        return (
-            <Col md={12} lg={10} xl={10}>
-                <MainProductBox>
-                    <DropdownCategory/>
-                    <ProductBox>
-                    {
-                        CategoryData.map(function(item){
-                            return <CategoryCard key={item.id}/>
-                        })
-                    }
-                    </ProductBox>
-                </MainProductBox>
-            </Col>
-        );
+            let {isLoaded, items} = this.state;
+    
+            if (!isLoaded) {
+                return <div>Loading... </div>
+        }
+          
+            else {
+            return (
+                <Col md={12} lg={10} xl={10}>
+                    <MainProductBox>
+                        <DropdownCategory/>
+                        <ProductBox>
+                        {
+                            items.map(function(item){
+                                return <div key={item.id}>{item.id}</div>
+                            })
+                        }
+                        </ProductBox>
+                    </MainProductBox>
+                </Col>
+            );
+        }
+        
     }
 }
 
-
-
-
-
-
+// export default class ProductCardBox extends Component {
+//     render() {
+//         return (
+//             <Col md={12} lg={10} xl={10}>
+//                 <MainProductBox>
+//                     <DropdownCategory/>
+//                     <ProductBox>
+//                     {
+//                         CategoryData.map(function(item){
+//                             return <CategoryCard key={item.id}/>
+//                         })
+//                     }
+//                     </ProductBox>
+//                 </MainProductBox>
+//             </Col>
+//         );
+//     }
+// }
